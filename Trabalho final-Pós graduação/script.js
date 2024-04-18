4c2e46358fd9133592464ee3bca498ddb8b3d98d
Rita-Vicente <167468024+Rita-Vicente@users.noreply.github.com>
2024-04-18 23:31:42 +0100

    Update script.js

--- Trabalho final-Pós graduação/script.js
+++ Trabalho final-Pós graduação/script.js
@@ -1,11 +1,26 @@
-<script>
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function moveSlide(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("carousel-slide")[0].getElementsByTagName("img");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}
+
 function toggleSignupForm() {
     var form = document.getElementById('signupForm');
-    if (form.style.display === 'none' || form.style.display === '') {
-        form.style.display = 'block';
+    if (form.style.display === 'none') {
+      form.style.display = 'block';
     } else {
-        form.style.display = 'none';
+      form.style.display = 'none';
     }
-}
-</script>
-
+  }
